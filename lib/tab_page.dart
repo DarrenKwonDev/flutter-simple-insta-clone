675e8ec89e9c800630aa31455861877df373037b
DarrenKwonDev <anatomy1545@gmail.com>
2020-05-14 08:13:21 +0900

    initial commit. create bottombar

---
+++ lib/tab_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // 하단 바텀 바 클릭시 적용할 함수
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _onItemTapped,
+          currentIndex: _selectedIndex,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text("Home")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), title: Text("search")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), title: Text("Account")),
+          ]),
+    );
+  }
+}
