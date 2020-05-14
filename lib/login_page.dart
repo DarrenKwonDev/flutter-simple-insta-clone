3a29b162b896fe5a62f72deeaf77a6bd847fbb1f
DarrenKwonDev <anatomy1545@gmail.com>
2020-05-14 22:46:51 +0900

    google authentication

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatelessWidget {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,10 +17,30 @@ class LoginPage extends StatelessWidget {
             Text("Instagram",
                 style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
             Padding(padding: EdgeInsets.all(20.0)),
-            SignInButton(Buttons.Google, onPressed: () {})
+            SignInButton(Buttons.Google, onPressed: () {
+              _handleSignIn().then((user) {
+                print(user);
+              });
+            })
           ],
         ),
       ),
     );
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    print("signed in " + user.displayName);
+    return user;
   }
 }
